39b4fdcaa0e6798c59c63d118b005229d2a4af82
Kim Seonmin <w.hand715@gmail.com>
2024-01-22 21:52:49 +0900

    solve 24263

---
+++ src/timecomplex/_n24263_.java
@@ -0,0 +1,33 @@
+package timecomplex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _n24263_ {
+
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n + 1];
+        MenOfPassion(A, n);
+
+        System.out.println(count);
+        System.out.println(1);
+    }
+
+    public static int MenOfPassion(int[] A, int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += A[i]; // 코드1
+            count++;
+        }
+        return sum;
+    }
+}
+/*
+다항식으로 나타내면 N 이므로 최고 차항은 1
+ */
