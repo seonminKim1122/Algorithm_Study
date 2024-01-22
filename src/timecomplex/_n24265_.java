951034e10b2e443af02a8870efcd315ee249e90a
Kim Seonmin <w.hand715@gmail.com>
2024-01-22 22:03:35 +0900

    solve 24265

---
+++ src/timecomplex/_n24265_.java
@@ -0,0 +1,27 @@
+package timecomplex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _n24265_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((long) n * (n - 1) / 2);
+        System.out.println(2);
+    }
+}
+/*
+1 <= i <= n-1
+i + 1 <= j <= n
+
+2 <= <= n
+3 <= <= n
+...
+n <= <= n
+
+1 + 2 + 3 + 4 + ... + n-1
+=> (n-1)*n / 2
+ */
