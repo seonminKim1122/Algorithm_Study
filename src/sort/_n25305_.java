3470043441be90e9b7cd0ddc0a4e1a1b31e1cbd9
Kim Seonmin <w.hand715@gmail.com>
2024-01-24 21:39:36 +0900

    solve 25305

---
+++ src/sort/_n25305_.java
@@ -0,0 +1,46 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.lang.StringBuilder;
+
+public class _n25305_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] sortedNumbers = bubbleSort(numbers);
+        System.out.println(sortedNumbers[N - k]);
+    }
+
+    public static int[] bubbleSort(int[] numbers) {
+        int N = numbers.length;
+        
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }    
+        }
+        
+        return numbers;
+    }
+}
+/*
+버블 정렬 -> 시간복잡도 O(N^2)
+ */
