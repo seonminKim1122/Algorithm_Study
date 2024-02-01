99c3a7ba66ca25a1f03d086e33064a695a6e429c
김선민 <w.hand715@gmail.com>
2024-02-02 08:49:59 +0900

    solve 15650

--- src/BackTracking/_n15650_.java
+++ src/BackTracking/_n15650_.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class _n15649_ {
-
-    static int[] path;
-    static boolean[] via;
-    static StringBuilder answer = new StringBuilder();
+public class _n15650_ {
 
     static int N;
     static int M;
+
+    static int[] path;
+    static StringBuilder answer = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,14 +21,13 @@ public class _n15649_ {
         M = Integer.parseInt(st.nextToken());
 
         path = new int[M];
-        via = new boolean[N + 1];
 
-        backTracking(0);
+        permutation(0, 1);
 
         System.out.println(answer);
     }
 
-    static void backTracking(int depth) {
+    static void permutation(int depth, int start) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
                 answer.append(path[i]).append(' ');
@@ -38,20 +36,17 @@ public class _n15649_ {
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (via[i]) continue;
-            via[i] = true;
+        for (int i = start; i <= N; i++) {
             path[depth] = i;
-            backTracking(depth + 1);
-            via[i] = false;
+            permutation(depth + 1, i + 1);
             path[depth] = 0;
         }
     }
 }
+
 /*
 의사코드
-1. 백트래킹 방식을 이용해 탐색한 depth == M 이 되는 경우 더 탐색을 하지 않고 현재까지 지나온 노드를 저장한 후
-2. 다시 이전 노드로 돌아가는 방식을 통해 문제에서 요구하는 값을 구한다.
+1. 중복되는 수열을 출력하면 안되므로 1번 노드부터 탐색을 시작했다면 2번을 첫 노드로 할 때는 1번을 탐색하지 않도록 한다.
 
 시간복잡도
 -> O(N!)
