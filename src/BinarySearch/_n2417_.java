d8d6de542203530f6bcc7b18a0d6389aeb22ac88
Kim Seonmin <w.hand715@gmail.com>
2024-02-04 11:08:14 +0900

    solve 9466

--- src/BinarySearch/_n2417_.java
+++ src/BinarySearch/_n2417_.java
@@ -27,3 +27,13 @@ public class _n2417_ {
         System.out.println(start);
     }
 }
+/*
+의사코드
+1. n의 제곱근은 반드시 n 보다 작기 때문에 1 ~ n 까지의 범위에 대해 Binary Search 를 수행한다.
+2. 다만 n 의 최대값이 2^63 - 1 이기 때문에 초기 mid 값은 약 2^62승이고 이를 제곱하면 long 의 표현 범위를
+넘어가게 되므로 계산과정에서 double 형으로의 변경이 필요하다.
+
+시간 복잡도
+O(logN)
+-> 0 ~ N 까지의 범위에 대해 BinarySearch 를 수행하기 때문에 O(logN)
+ */
