51379fae1506d2350c5416f078f8e3f3947e7af7
김선민 <w.hand715@gmail.com>
2024-02-07 10:12:09 +0900

    solve 1929

---
+++ src/NumberTheory/_n1929_.java
@@ -0,0 +1,39 @@
+package NumberTheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _n1929_ {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        boolean[] isNotPrime = new boolean[end + 1];
+        isNotPrime[1] = true;
+        for (int i = 2; i <= end; i++) {
+            if (isNotPrime[i]) continue;
+            for (int j = i * 2; j <= end; j += i) {
+                isNotPrime[j] = true;
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if (!isNotPrime[i]) {
+                answer.append(i).append('\n');
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
+/*
+에라토스테네스의 체로 풀이
+O(Nlog(logN))
+ */
